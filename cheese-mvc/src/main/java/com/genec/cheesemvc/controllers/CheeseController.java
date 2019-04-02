e4b37810448b230634bfb5b38efe0a6b538c8a97
GeneCPChung <genecpc@gmail.com>
2019-04-01 21:31:38 -0400

    removed violations

--- cheese-mvc/src/main/java/com/genec/cheesemvc/controllers/CheeseController.java
+++ cheese-mvc/src/main/java/com/genec/cheesemvc/controllers/CheeseController.java
@@ -1,24 +1,25 @@
 package com.genec.cheesemvc.controllers;
+
+import com.genec.cheesemvc.models.Cheese;
+import com.genec.cheesemvc.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-        static ArrayList<String> cheeses = new ArrayList<>();
+
 
         //Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model){
 
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title","My Cheeses");
 
         return "cheese/index";
@@ -31,11 +32,29 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName){
-        cheeses.add(cheeseName);
-
+    public String processAddCheeseForm(@RequestParam String cheeseName,
+        @RequestParam String cheeseDescription){
+        Cheese newCheese = new Cheese(cheeseName, cheeseDescription);
+        CheeseData.add(newCheese);
         // Redirect to /cheese
         return "redirect:";
     }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model){
+        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("title", "Remove Cheese");
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds){
+
+        for (int cheeseId : cheeseIds){
+            CheeseData.remove(cheeseId);
+        }
+        return "redirect:";
+    }
+
 
 }
